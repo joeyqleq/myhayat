68fe7358a3762011205f7f6982bde797c99f455f
joeyq <jq@joeyq.tech>
2025-02-26 17:48:50 +0200

    repo migration from eoj

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,15 @@ document.querySelector('#openModalButton').addEventListener('click', () => {
 
 document.querySelector('#closeModalButton').addEventListener('click', closeModal);
 
+document.addEventListener("DOMContentLoaded", function() {
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+      // Remove active class from all links
+      navLinks.forEach(nav => nav.classList.remove('active'));
+      // Add active class to the clicked link
+      this.classList.add('active');
+    });
+  });
+});
